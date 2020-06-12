f85873da501eeb2c480a1453ce98724101f3b8e1
nimishagarwal76 <nimishagarwal76@gmail.com>
2020-06-12 18:34:48 +0530

    Get form status

--- src/components/FormList.js
+++ src/components/FormList.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const FormList = () => {
+    const [formStatus, setformStatus] = useState({});
+    let data = {}
+    useEffect(() => {
+        try {
+            const getFormStatus = async () => {
+                let formFilled = await axios.get('/api/v1/forms/arrear');
+                formFilled = formFilled.data.data;
+                setformStatus({
+                    ...formFilled
+                })
+            }
+            getFormStatus();
+        } catch (err) {
+            console.log(err)
+        }
+    }, [])
+
+    let formTypes = ['reassesment', 'redo', 'formattive-assesment'];
     return (
         <div>
-            <li>Form 1</li>
-            <li>Form 2</li>
-            <li>Form 3</li>
+            {
+                formTypes.map(form => (
+                    formStatus[form] ?
+                    <li>{form}</li>:
+                    <li><b>{form}</b></li>
+                ))
+            }
         </div>
     )
 }
