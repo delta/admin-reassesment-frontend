b41544b1ad328a691ead18b2a042fdc9a8ce5278
nimishagarwal76 <nimishagarwal76@gmail.com>
2020-06-15 00:22:07 +0530

    Add logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,8 @@ import {
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom'
 import {isLogin} from '../utils/authUtils'
+import axios from "axios";
+import { unsetToken } from '../utils/authUtils'
 
 const loggedInUrls = [
     {
@@ -28,6 +30,21 @@ const loggedOutUrls = [
 
 export const CustomNavbar = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(isLogin())
+    const handleLogout = async () => {
+        let res = await axios({
+            method: 'post',
+            url: '/auth/logout',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Credentials': true
+            }
+        })
+        if(res.data.success) {
+            unsetToken();
+            window.location.href = '/';
+        }
+    }
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Navbar.Brand><Link to='/' style={{ textDecoration: 'none', color:'#ffffff'}}>ARREARS</Link></Navbar.Brand>
@@ -37,6 +54,9 @@ export const CustomNavbar = () => {
                     {(isLoggedIn ? loggedInUrls : loggedOutUrls).map((url, idx) => (
                             <Nav.Link as={Link} to={url.url} key={idx}>{url.name}</Nav.Link>
                     ))}
+                    {
+                        isLoggedIn?(<Nav.Link onClick={handleLogout}>Logout</Nav.Link>):''
+                    }
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
