952e8e048fe08f245afe2ffe3da21be4121fbf87
nimishagarwal76 <nimishagarwal76@gmail.com>
2020-06-16 21:41:39 +0530

    Remove Dockerfile

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const isLogin = () => {
-    return !localStorage.getItem('tokens')
+    return !!localStorage.getItem('tokens')
 }
 
 export const unsetToken = () => {
