277ea6501cba611b1af756029d647b3b57c8bef0
KIRA009 <shohanduttaroy99@gmail.com>
2020-06-18 23:13:52 +0530

    [feat] Add fields for masters course
    Added following
    - Add specialisation and degree field for masters course
    
    Changed following
    
    Fixed following

---
+++ src/components/forms/constants.js
@@ -0,0 +1,37 @@
+export const departmentList = ['Architecture', 'Chemical Engineering', 'Civil Engineering', 'Computer Science and Engineering', 'Electrical and Electronics Engineering', 'Electronics and Communication Engineering', 'Instrumentation and Control Engineering', 'Mechanical Engineering', 'Metallurgical and Materials Engineering', 'Production Engineering'];
+export const statusList = ['Passing Out', 'Passed Out']
+export const degreeList = ['M.Tech', 'M.Arch', 'M.Sc', 'MBA', 'MCA'];
+export const masterSemesterOptions = {
+    'M.Tech': 4,
+    'M.Arch': 4,
+    'M.Sc': 4,
+    'MCA': 6,
+    'MBA': 6
+};
+const startBatch = 2011;
+const endBatch = 2016;
+
+export let batchOptions = [];
+for (let i = startBatch; i <= endBatch; ++i) batchOptions.push(i);
+
+export let semesterOptions = [];
+for (let i = 1; i <= 10; ++i) semesterOptions.push(i);
+
+export const specialisations = {
+    'M.Tech': [
+        'Energy Engineering', 'Chemical Engineering', 'Process Control and Instrumentation', 'Industial Automation',
+        'Transportation Engineering and Management', 'Construction Technology and Management', 'Structural Engineering',
+        'Environmental Engineering', 'Computer Science and Engineering', 'Power Electronics', 'Power Systems',
+        'Communication Systems', 'VLSI System', 'Industrial Safety Engineering', 'Thermal Power Engineering',
+        'Material science and Engineering', 'Industrial metallurgy', 'Welding Engineering',
+        'Industrial Engineering and Management', 'Manufacturing Technology', 'Non-Destructive Testing',
+        'Data Analytics'
+    ],
+    'M.Arch': [
+        'Energy Efficient and Sustainable Architecture'
+    ],
+    'M.Sc': [
+        'Computer Science', 'Chemistry', 'Physics', 'Mathematics'
+    ],
+    'MCA': [], 'MBA': []
+}
